8d08c4ce513ebbe52a35aa664b31609f0574e086
Yongwen Zhuang <zyeoman@163.com>
2016-04-08 17:54:46 +0800

    add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+#
+# encoding=utf8
+
+from distutils.core import setup
+
+setup(name="thu",
+      version="1.0.0",
+      description="Login,logout,check tsinghua net",
+      author="zyeoman",
+      author_email="zyeoman@163.com",
+      url="https://github.com/zyeoman/thu",
+      packages=['thu']
+      )
