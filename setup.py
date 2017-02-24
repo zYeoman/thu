fe92422138694fb5149afe78a9e84f31b4d5873c
Yongwen Zhuang <zyeoman@163.com>
2017-02-24 17:46:07 +0800

    :sparkles: Add scripts in setup.py

--- setup.py
+++ setup.py
@@ -4,10 +4,11 @@
 from distutils.core import setup
 
 setup(name="thu",
-      version="1.0.0",
+      version="1.0.1",
       description="Login,logout,check tsinghua net",
       author="zyeoman",
       author_email="zyeoman@163.com",
       url="https://github.com/zyeoman/thu",
-      packages=['thu']
+      packages=['thu'],
+      scripts=['scripts/thu', 'scripts/thu.bat']
       )
