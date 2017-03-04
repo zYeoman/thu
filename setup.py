e93c87141658e5a68d0fb8001d9416092f108f11
Yongwen Zhuang <zyeoman@163.com>
2017-03-04 15:01:43 +0000

    :boom: Setup.py with dependencies

--- setup.py
+++ setup.py
@@ -1,14 +1,18 @@
 #
 # encoding=utf8
 
-from distutils.core import setup
+from setuptools import setup
 
 setup(name="thu",
-      version="1.0.1",
+      version="1.0.2",
       description="Login,logout,check tsinghua net",
       author="zyeoman",
       author_email="zyeoman@163.com",
       url="https://github.com/zyeoman/thu",
       packages=['thu'],
+      install_requires=[
+          'bs4',
+          'prettytable',
+      ],
       scripts=['scripts/thu', 'scripts/thu.bat']
       )
